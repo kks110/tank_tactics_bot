2a943d9165a8a4a02f9f79243ac7ea0d095f62e9
Kelvin Samuel <kelvin@kks110.com>
2023-06-30 14:05:41 +0100

    Update battle_log.rb

--- battle_log.rb
+++ battle_log.rb
@@ -2,7 +2,8 @@ require 'logger'
 
 class BattleLog
   def self.logger
-    logger = Logger.new('battle_log.txt')
+    log_path = ENV.fetch('LOG_PATH', 'battle_log.txt')
+    logger = Logger.new(log_path)
     logger.formatter = proc { |_, datetime, _, msg| "#{datetime}, #{msg}\n" }
     logger.datetime_format = '%Y-%m-%d %H:%M'
     logger
