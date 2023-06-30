2bb0cf8a29296aaeb66a472f04cb2ab67a6ff890
Kelvin Samuel <kelvin@kks110.com>
2023-06-30 14:35:23 +0100

    Update initialise.rb

--- initialise.rb
+++ initialise.rb
@@ -5,7 +5,7 @@ require 'active_record'
 
 env = ENV.fetch('ENVIRONMENT', 'dev')
 
-db_config_file = File.open('config/database.yml')
+db_config_file = File.open(File.expand_path('config/database.yml', __dir__))
 db_config = YAML.safe_load(db_config_file)[env]
 
 db_config.merge!('url' => ENV.fetch('TT_DATABASE_URL', nil)) if env == 'prod'
