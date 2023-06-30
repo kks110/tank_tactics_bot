0d4b8e28cce090733c0bddc9847939cc5012774a
Kelvin Samuel <kelvin@kks110.com>
2023-06-30 12:51:47 +0100

    separate prod and dev db config

--- initialise.rb
+++ initialise.rb
@@ -3,7 +3,7 @@ require 'dotenv/load'
 require 'yaml'
 require 'active_record'
 
-db_config_file = File.open('./config/database.yml')
-db_config = YAML::load(db_config_file)
+db_config_file = File.open('./config/database.yml', aliases: true)
+db_config = YAML.safe_load(db_config_file)[ENV.fetch('ENVIRONMENT', 'dev')]
 
 ActiveRecord::Base.establish_connection(db_config)
