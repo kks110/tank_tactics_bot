f5ea03e51a758619b4e2b501e0137efe5696aacb
Kelvin Samuel <kelvin.samuel@simplybusiness.co.uk>
2023-07-03 10:23:13 +0100

    move todos

--- run.rb
+++ run.rb
@@ -19,23 +19,3 @@ Command::LIST.each do |command|
 end
 
 bot.run
-
-
-# TODO: automate energy distribution 
-# TODO: Show tank range
-# TODO: Look in to better random distributions
-# TODO: Have some kind of game state. Started, winner, that kind of thing and add a check after each command to see if there is a winner
-# TODO: Run rubocop
-# TODO: Reset game
-# TODO: Use config for board sizes (or make optional at game start?)
-# TODO: Use config for energy costs (or make optional at game start?)
-# TODO: Come up with some kind of standard notation, and write something that can play out the the game a step at a time
-# TODO: Tests?
-# TODO: Look at image generation for the board
-
-# For next game
-# TODO: Have random heart spawn when daily energy is given
-# TODO: Make board 'round'
-
-# Updates in this patch:
-# Mentions on daily update and on being shot
