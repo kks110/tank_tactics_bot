ac2c5ceec6d0b8d87d12032a1b251d2816172f50
Kelvin Samuel <kelvin@kks110.com>
2023-07-27 17:26:09 +0100

    add log path to battle log

--- battle_log.rb
+++ battle_log.rb
@@ -2,10 +2,13 @@ require 'logger'
 
 class BattleLog
   def self.logger
-    log_path = ENV.fetch('LOG_PATH', 'battle_log.txt')
     logger = Logger.new(log_path)
     logger.formatter = proc { |_, datetime, _, msg| "#{datetime}, #{msg}\n" }
     logger.datetime_format = '%Y-%m-%d %H:%M'
     logger
+  end
+
+  def self.log_path
+    ENV.fetch('LOG_PATH', 'battle_log.txt')
   end
 end
