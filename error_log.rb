ccf12081bc635105be76db17ee85ae9a50bd7e18
Kelvin Samuel <kelvin.samuel@simplybusiness.co.uk>
2023-08-09 11:03:06 +0100

    fix some bugs

--- error_log.rb
+++ error_log.rb
@@ -2,13 +2,14 @@ require 'logger'
 
 class ErrorLog
   def self.logger
-    logger = Logger.new(log_path)
+    path = "#{log_path}error_log.txt"
+    logger = Logger.new(path)
     logger.formatter = proc { |_, datetime, _, msg| "#{datetime}, #{msg}\n" }
     logger.datetime_format = '%Y-%m-%d %H:%M'
     logger
   end
 
   def self.log_path
-    ENV.fetch('LOG_PATH', 'error_log.txt')
+    ENV.fetch('LOG_PATH', './')
   end
 end
