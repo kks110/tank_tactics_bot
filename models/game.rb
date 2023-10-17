4317a39f2eb7beb18781d9add34fd1c8689e4894
Kelvin Samuel <kelvin.samuel@simplybusiness.co.uk>
2023-10-17 15:07:16 +0100

    Update game.rb

--- models/game.rb
+++ models/game.rb
@@ -3,10 +3,4 @@ require 'active_record'
 class Game < ActiveRecord::Base
   validates :server_id, :max_x, :max_y, presence: true
   validates :server_id, uniqueness: true
-
-  validates :cities, inclusion: [true, false]
-  validates :cities, exclusion: [nil]
-
-  validates :fog_of_war, inclusion: [true, false]
-  validates :fog_of_war, exclusion: [nil]
 end
